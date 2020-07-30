e34ee4247e81e8d7d37e6df08263aaaee136e535
罗戚洪 <474693310@qq.com>
2020-07-30 09:44:35 +0800

    'default'

--- package/src/components/base/Dialog.vue
+++ package/src/components/base/Dialog.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="dialog">
-        <slot></slot>
-    </div>
+  <div class="dialog">
+    <slot></slot>
+  </div>
 </template>
 <style lang="scss" scoped>
 .dialog {
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.5);
-    position: fixed;
-    top: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
